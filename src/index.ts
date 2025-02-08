71ac74ebe285da98ec3596029207aee67a117e04
Hiroki Chiyoda <contact@arafipro.com>
2025-02-08 21:05:15 +0900

    04_embeddings

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
+import type { Ai, VectorizeIndex } from "@cloudflare/workers-types";
+import { CloudflareWorkersAIEmbeddings } from "@langchain/cloudflare";
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 
-const app = new Hono();
+type Env = {
+  AI: Ai;
+  VECTORIZE: VectorizeIndex;
+};
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
+const app = new Hono<{ Bindings: Env }>();
+
+app.use("*", cors());
+app.get("/", async (c) => {
+  const text =
+    "2024年の日本シリーズは、横浜DeNAベイスターズ（以下、DeNA）と福岡ソフトバンクホークス（以下、ソフトバンク）による第75回日本選手権シリーズ。";
+  const embeddings = new CloudflareWorkersAIEmbeddings({
+    binding: c.env.AI,
+    modelName: "@cf/baai/bge-base-en-v1.5",
+  });
+  const VectorizeData = await embeddings.embedQuery(text);
+
+  return c.json(VectorizeData);
 });
 
 export default app;
