9f3e19907c56483a145318cc1347f0ca7f13362e
Hiroki Chiyoda <contact@arafipro.com>
2025-02-09 07:13:54 +0900

    06_create-vectorizestore-func

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import type { Ai } from "@cloudflare/workers-types";
 import {
   CloudflareVectorizeStore,
   CloudflareWorkersAIEmbeddings,
@@ -8,17 +9,22 @@ import { Env } from "./types";
 
 const app = new Hono<{ Bindings: Env }>();
 
+function VectorizeStore(ai: Ai, vectorize: VectorizeIndex) {
+  const embeddings = new CloudflareWorkersAIEmbeddings({
+    binding: ai,
+    modelName: "@cf/baai/bge-base-en-v1.5",
+  });
+  const store = new CloudflareVectorizeStore(embeddings, {
+    index: vectorize,
+  });
+  return store;
+}
+
 app.use("*", cors());
 app.get("/", async (c) => {
   const text =
     "2024年の日本シリーズは、横浜DeNAベイスターズ（以下、DeNA）と福岡ソフトバンクホークス（以下、ソフトバンク）による第75回日本選手権シリーズ。";
-  const embeddings = new CloudflareWorkersAIEmbeddings({
-    binding: c.env.AI,
-    modelName: "@cf/baai/bge-base-en-v1.5",
-  });
-  const store = new CloudflareVectorizeStore(embeddings, {
-    index: c.env.VECTORIZE,
-  });
+  const store = VectorizeStore(c.env.AI, c.env.VECTORIZE);
   const results = await store.similaritySearch("どこが優勝した？", 3);
 
   return c.json(results);
