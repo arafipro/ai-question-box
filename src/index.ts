36f1dcbc392e9cc39b7885dca1afd5a937f60601
Hiroki Chiyoda <contact@arafipro.com>
2025-02-09 08:28:49 +0900

    07_add-vector-database

--- src/index.ts
+++ src/index.ts
@@ -29,5 +29,29 @@ app.get("/", async (c) => {
 
   return c.json(results);
 });
+app.get("/add", async (c) => {
+  const store = VectorizeStore(c.env.AI, c.env.VECTORIZE);
+  await store.addDocuments(
+    [
+      {
+        pageContent:
+          "2024年の日本シリーズは、横浜DeNAベイスターズ（以下、DeNA）と福岡ソフトバンクホークス（以下、ソフトバンク）による第75回日本選手権シリーズ",
+        metadata: {},
+      },
+      {
+        pageContent: "2024年10月26日に開幕し、11月3日の第6戦まで行われた。",
+        metadata: {},
+      },
+      {
+        pageContent:
+          "DeNAが4勝2敗でソフトバンクを破り、26年ぶり3度目の日本一に輝いた。",
+        metadata: {},
+      },
+    ],
+    { ids: ["id1", "id2", "id3"] }
+  );
+
+  return c.json({ success: true });
+});
 
 export default app;
